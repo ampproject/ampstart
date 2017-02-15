e8b5049288218161b4029cfb16948cef6fe09646
Sriram Krishnan <camelburrito@users.noreply.github.com>
2017-02-14 19:20:12 -0800

    Making code highlighting work (#44)

--- tasks/validate.js
+++ tasks/validate.js
@@ -23,7 +23,7 @@ function validate() {
         `${config.dest.templates}/**/*.html`,
         `${config.dest.www_pages}/**/*.html`,
         `!${config.dest.templates}/components/**/*.html`,
-        `!${config.dest.www_pages}/hl-partials/**/*.html`,
+        `!${config.dest.hl_partials}/**/*.html`,
       ])
       .pipe(validator.validate())
       .pipe(validator.format());
