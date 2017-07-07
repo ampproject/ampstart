6198b00ec83392434de5f458162668120621cb2b
Aaron Turner <aaron@aaronthedev.com>
2017-07-07 16:04:39 -0700

    Prefixed, and moved to <code> tags

--- tasks/escape.js
+++ tasks/escape.js
@@ -23,7 +23,7 @@ const through = require('through2');
 function escape() {
   return gulp.src(config.src.hl_partials)
     .pipe(rename(function(path) {
-      path.basename += "-esc";
+      path.basename = `esc-${path.basename}`;
     }))
     .pipe(through.obj(function(file, enc, cb) {
       if (file.isNull()) {
