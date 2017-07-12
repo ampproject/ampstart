3a950525fe18f8173a7ce0b469b9407328877dda
Gurmukh Panesar <gurmukhp@gmail.com>
2017-07-12 18:19:22 +0100

    Commerce - Product details template (#500)
    * Init product details
    
    * Product Details - Layout for wide view
    
    * Tidy up CSS
    
    * Add empty lines
    
    * Tidy up rating component
    
    * Update headers, deprecate vertical-table and add comments.
    
    - Replace vertical table with dl
    - Ensure headers are in a correct order semantically
    - Add responsive header addon and remove custom header classes
    - Additional comments
    
    * fix image paths
    
    * Add revision comment for responsive spaces.

--- tasks/config.js
+++ tasks/config.js
@@ -19,7 +19,7 @@ module.exports = {
     components: 'components/**/*.*',
     templates: [
       '{templates, dummy}/**/*.html',
-      '!templates/e-commerce/partials/hero/*.html'
+      '!templates/*/partials/*.html'
     ],
     www_pages: 'www/**/*.html',
     hl_partials: 'hl-partials/**/*.html',
