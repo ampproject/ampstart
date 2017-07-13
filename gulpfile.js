d6e693c9a82d502774b6fd64f268bc76fe45edbc
Aaron Turner <aaron@aaronthedev.com>
2017-07-13 11:44:26 -0700

    Made requested changes by @camelburrito

--- gulpfile.js
+++ gulpfile.js
@@ -163,18 +163,7 @@ gulp.task('www', function() {
       .pipe(gulp.dest(config.dest.www_pages))
 });
 
-gulp.task('watch:www', 'watch stuff, similar to watchdev, but includes bundling and validation', ['build'], function() {
-  return gulp.watch(
-      [
-        config.src.components, config.src.templates, config.src.www_pages,
-        config.src.css, config.src.data, config.src.img
-      ],
-      function(event) {
-        runSequence(['img', 'postcss'], ['www', 'posthtml'], 'validate', 'bundle');
-      });
-});
-
-gulp.task('watch:dev', 'watch stuff, minimal watching for development', ['build'], function() {
+gulp.task('watch:www', 'watch stuff, minimal watching for development', ['build'], function() {
   return gulp.watch(
       [
         config.src.components, config.src.templates, config.src.www_pages,
@@ -222,13 +211,6 @@ gulp.task('postcss', 'build postcss files', function() {
 });
 
 gulp.task('serve', 'Host a livereloading webserver for the project', ['watch:www'], function() {
-  gulp.src(config.dest.default).pipe(server({
-    livereload: true,
-    directoryListing: {enable: true, path: 'dist'},
-  }));
-});
-
-gulp.task('serve:dev', 'Host a livereloading webserver for the project', ['watch:dev'], function() {
   gulp.src(config.dest.default).pipe(server({
     livereload: true,
     directoryListing: {enable: true, path: 'dist'},
