521079a3d6433824bf10d3e6ed05b8e7f706505d
raisaveuc <raisa.cuevas@gmail.com>
2017-08-02 17:56:40 +0100

    [Commerce] Add focus to navbar trigger, update copy (#571)
    * [Commerce] Add carousel buttons
    
    * [Commerce] Add focus to navbar menu icon, move classes to JSON
    
    * [Commerce] Add focus to navbar menu icon, move classes to JSON
    
    * [Commerce] Move nabber classes to shared json
    
    * [Commerce] Update placeholder text
    
    * [Commerce] Corrects related product titles
    
    * [Commerce] Match textarea color
    
    * [Commerce] Add file overview for build.js, clean up functions
    
    * [Commerce] Correct build.js JSON output

--- templates/e-commerce/api/build.js
+++ templates/e-commerce/api/build.js
@@ -1,10 +1,46 @@
+/*
+ * Builds a collection of products and writes JSON files sorted by price and 
+ * category.
+ */
+
 const fs = require('fs');
 const path = require('path');
 
 const currentWorkingDirectory = path.dirname(process.argv[1]);
 const totalProducts = 12;
-const title = 'Product Name';
-const subtitle = 'Mauris tincidunt.';
+const titles = {
+  'gloves': {
+    'name': 'Pointer gloves',
+    'description': 'Fingerless with lightweight construction'
+  },
+  'cap': {
+    'name': 'Farringdon cap',
+    'description': 'Water resistant and breathable'
+  },
+  'bike1': {
+    'name': 'Highland',
+    'description': 'Timeless style in Reynolds 531 tubing'
+  },
+  'bike2': {
+    'name': 'Estuary',
+    'description': 'Relaxed geometry for comfortable commuting'
+  },
+  'bike3': {
+    'name': 'Beacon',
+    'description': 'Full carbon frameset, designed to climb'
+  },
+  'bike4':{
+    'name': 'Herne Hill',
+    'description': 'Built for the track, at home on the street'
+  },
+  'bike5': {
+    'name': 'Pennine',
+    'description': 'Full-featured tourer for long-distance rides'
+  }
+};
+const titleListing = [
+  titles.bike1, titles.bike2, titles.bike3, titles.gloves, titles.cap, titles.gloves, titles.cap, titles.gloves, titles.cap, titles.gloves, titles.cap, titles.bike4
+];
 const minPrice = 400;
 const maxPrice = 999;
 const imagesFolder = '/img/e-commerce/product/';
@@ -63,15 +99,17 @@ function lowToHigh(productA, productB) {
   return 0;
 }
 
-function saveProducts() {
-  let fileName =
-      `${currentWorkingDirectory}/${highToLowName}-all-${outputFile}`;
-  products.items.sort(highToLow);
-  fs.writeFileSync(fileName, JSON.stringify(products, null, '\t'));
+function writeFile(sortName, sortFunction, products, categoryName = 'all') {
+  products.sort(sortFunction);
+  let data = {items: products};
 
-  products.items.sort(lowToHigh);
-  fileName = `${currentWorkingDirectory}/${lowToHighName}-all-${outputFile}`;
-  fs.writeFileSync(fileName, JSON.stringify(products, null, '\t'));
+  let fileName = `${currentWorkingDirectory}/${sortName}-${categoryName}-${outputFile}`;
+  fs.writeFileSync(fileName, JSON.stringify(data, null, '\t')); 
+}
+
+function saveProducts() {
+  writeFile(highToLowName, highToLow, products.items);
+  writeFile(lowToHighName, lowToHigh, products.items);
 }
 
 function saveCategories() {
@@ -80,29 +118,18 @@ function saveCategories() {
   });
 
   categories.forEach((categoryName) => {
-    productCategories[categoryName].sort(highToLow);
-    let data = {items: productCategories[categoryName]};
-
-    let fileName = `${currentWorkingDirectory}/${
-                                                 highToLowName
-                                               }-${categoryName}-${outputFile}`;
-    fs.writeFileSync(fileName, JSON.stringify(data, null, '\t'));
-
-    productCategories[categoryName].sort(lowToHigh);
-    data = {items: productCategories[categoryName]};
-
-    fileName = `${currentWorkingDirectory}/${lowToHighName}-${
-                                                              categoryName
-                                                            }-${outputFile}`;
-    fs.writeFileSync(fileName, JSON.stringify(data, null, '\t'));
+    writeFile(highToLowName, highToLow, productCategories[categoryName],
+        categoryName);
+    writeFile(lowToHighName, lowToHigh, productCategories[categoryName],
+        categoryName);
   });
 }
 
 function generateProducts() {
   for (let i = 0; i < totalProducts; i++) {
     products.items.push({
-      name: title,
-      description: subtitle,
+      name: titleListing[i].name,
+      description: titleListing[i].description,
       price: getRandomPrice(),
       image: getNextImage(),
       category: getRandomCategory()
